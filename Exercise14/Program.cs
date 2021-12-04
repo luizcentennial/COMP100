909e1cc67b0c7ac01438f77a570dd4efbf0ae8b5
Luiz Parente <lp.parente@gmail.com>
2021-12-03 22:31:24 -0500

    Methods.

---
+++ Exercise14/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Exercise14 {
+	class Program {
+		static void Main(string[] args) {
+			/*
+			 
+			Write a program that defines two methods.
+			
+			The first method, called GenerateArray
+			generates an array of random integers. It
+			takes in one integer parameter, which is the size
+			of the array to be generated.
+
+			The second method, called DisplayArray, 
+			prints all array elements in the console,
+			on the same line, separated by commas. 
+			It takes in one integer array (the array to be displayed)
+			as a parameter, and does not return anything.
+			
+			Test your methods by calling them in the Main method.
+			
+			*/
+
+			int[] array = GenerateArray(10);
+
+			DisplayArray(array);
+		}
+
+		public static int[] GenerateArray(uint size) {
+			Random randomizer = new Random();
+			int[] array = new int[size];
+
+			for (int i = 0; i < array.Length; i++) {
+				array[i] = randomizer.Next(100, 200);
+			}
+
+			return array;
+		}
+
+		public static void DisplayArray(int[] array) {
+			foreach (int item in array) {
+				Console.Write($"{item}, ");
+			}
+
+			Console.WriteLine('\n');
+		}
+	}
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Methods {
+	class Program {
+		static void Main(string[] args) {
+			// METHODS
+
+			// What are methods?
+
+			// Methods are procedures that execute a sequence of steps (algorithm),
+			// that have already been coded.
+			// They encapsulate a piece of code, and are stored in your program.
+
+			// One of the main goals is to keep your code organized.
+
+			// Examples:
+			// - The Main method.
+			// - Console.WriteLine();
+			// - "Some string".ToUpper();
+			// - int[] numbers = { 1, 2, 3, 4, 5 };
+			// - Array.Sort(numbers);
+
+			// Invoking the Greet method:
+			Greet();
+			Greet("Clark Kent");
+
+			// Invoking the Add method:
+			int sum = Add(1, 2);
+			Console.WriteLine($"The result is {sum}");
+
+			double addition = Add(1.5, 3.14);
+			Console.WriteLine($"The result is {addition:F3}");
+
+			// Invoking the Multiply method:
+			double multiplication = Multiply(1.71, 3.14);
+			Console.WriteLine($"The result is {multiplication}");
+
+			multiplication = Multiply(1.71);
+			Console.WriteLine($"The result is {multiplication}");
+		}
+
+		// METHOD DECLARATIONS
+		// There are many different ways to declare methods.
+		// Methods will always have:
+		// - A signature (header);
+		// - A body.
+
+		// Method signatures MUST have:
+		// - An access modifier (i.e. public, private);
+		// - A return type (i.e. string, int, void, etc);
+		// - A name (usually is descriptive of what the method does).
+
+		// Method signatures MAY have (dependent on use-case):
+		// - The static keyword;
+		// - Parameters.
+
+		// All methods are declared inside classes.
+
+		// Declaring a method that does not have a return value, and does not take in any parameters.
+		public static void Greet() { // <-- Signature
+			// Body
+			Console.WriteLine("Greet method says hello!");
+		}
+
+		// Declaring a method that does not have a return value, and takes in parameters:
+		// In C#, parameters are strongly typed.
+		public static void Greet(string name) { // <-- Overloading Greet() method. Overloaded methods are methods with multiple definitions.
+			Console.WriteLine($"Overloaded Greet method says hello, {name}!");
+		}
+
+		// Declaring a method that returns a value, and takes in parameters:
+		public static int Add(int number1, int number2) { // <-- Parameters are comma-separated.
+			int result = number1 + number2;
+
+			// Methods that have a return value MUST return something.
+			return result;
+		}
+
+		// Methods can return values of any type, and parameters can be of any type.
+		public static double Add(double number1, double number2) {
+			return number1 + number2;
+		}
+
+		// Declaring a method with optional parameters:
+		public static double Multiply(double number1, double number2 = 1.0) {
+			return number1 * number2;
+		}
+	}
+}
