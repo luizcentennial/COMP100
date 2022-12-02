9b0e33298b776673d5fda217aa9b52b2d9b925b7
Luiz Parente <lp.parente@gmail.com>
2022-12-01 20:07:56 -0500

    Params keyword.

---
+++ Example35/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Example35 {
+	class Program {
+		static void Main(string[] args) {
+			// PARAMS KEYWORD:
+			// The keyword "params" can be used as a modifier to method parameters.
+			// It makes it more convenient to invoke methods that take in multiple values.
+			// And it can be used an alternative to defining array arguments.
+
+			// Method CalculateAverage requires an array object as a parameter.
+			double[] array = { 0.5, 1.2, 3, 4, 5 };
+			double average = CalculateAverage(array);
+
+			Console.WriteLine($"The average with array is: {average}");
+
+			// Method CalculateAverageWithParams do not require an array object.
+			average = CalculateAverageWithParams(0.5, 1.2, 3, 4, 5);
+
+			Console.WriteLine($"The average with params is: {average}");
+		}
+
+		public static double CalculateAverage(double[] numbers) {
+			double sum = 0;
+
+			foreach (double number in numbers) {
+				sum += number;
+			}
+
+			return sum / numbers.Length;
+		}
+
+		public static double CalculateAverageWithParams(params double[] numbers) {
+			double sum = 0;
+
+			foreach (double number in numbers) {
+				sum += number;
+			}
+
+			return sum / numbers.Length;
+		}
+	}
+}
---
+++ Example36/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Example36 {
+	class Program {
+		static void Main(string[] args) {
+			/*
+			 
+			1. Create a method that returns an integer array with random numbers. The array size is expected as a parameter. 
+			2. Create a method that displays an array in the console. The expected format is: [ a, b, c, d, e, ... ]
+			
+			*/
+
+			// Creating a random array:
+			int[] numbers = GetRandomIntegerArray(5);
+
+			// Display numbers array in the console:
+			//Console.WriteLine(numbers); // This does not show the array elements.
+
+			// Instead, we need to call our own method:
+			DisplayArray(numbers);
+		}
+
+		public static int[] GetRandomIntegerArray(int size) {
+			Random randomizer = new Random();
+			int[] array = new int[size];
+
+			for (int i = 0; i < size; i++) {
+				array[i] = randomizer.Next(10);
+			}
+
+			return array;
+		}
+
+		public static void DisplayArray(int[] array) {
+			Console.Write("[ ");
+
+			for (int i = 0; i < array.Length; i++) {
+				if (i == array.Length - 1) {
+					Console.Write(array[i]);
+				}
+				else {
+					Console.Write($"{array[i]}, ");
+				}
+			}
+
+			Console.Write(" ]");
+
+			// The code below can replace all lines above.
+			//Console.WriteLine($"[ {string.Join(", ", array)} ]");
+		}
+	}
+}
