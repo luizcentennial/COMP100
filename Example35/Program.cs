73678ac7f424fc15a341fc7f5f1ec47cd0abaf8d
Luiz Parente <lp.parente@gmail.com>
2022-12-02 20:16:13 -0500

    Updating comments.

--- Example35/Program.cs
+++ Example35/Program.cs
@@ -6,7 +6,7 @@ namespace Example35 {
 			// PARAMS KEYWORD:
 			// The keyword "params" can be used as a modifier to method parameters.
 			// It makes it more convenient to invoke methods that take in multiple values.
-			// And it can be used an alternative to defining array arguments.
+			// And it can be used as an alternative to defining array arguments.
 
 			// Method CalculateAverage requires an array object as a parameter.
 			double[] array = { 0.5, 1.2, 3, 4, 5 };
@@ -14,7 +14,7 @@ namespace Example35 {
 
 			Console.WriteLine($"The average with array is: {average}");
 
-			// Method CalculateAverageWithParams do not require an array object.
+			// Method CalculateAverageWithParams does not require an array object.
 			average = CalculateAverageWithParams(0.5, 1.2, 3, 4, 5);
 
 			Console.WriteLine($"The average with params is: {average}");
