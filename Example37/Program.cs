8076a8c4c21e262890ed1c4e661950a558a669a7
Luiz Parente <lp.parente@gmail.com>
2022-12-08 21:23:20 -0500

    Hands on exercise.

---
+++ Example37/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Example37 {
+	class Program {
+		// SCOPES:
+		// A scope is a region in which an object exists.
+		// The scope of a local variable is the region in which it is declared.
+		// However, objects declared in parent scopes are accessible inside child scopes,
+		// even though the opposite is NOT true.
+
+		// It is possible to define variables at the class level.
+		// These variables will be accessible across all of this class' methods.
+		// In C#, these variables are called "fields".
+		static string classVariable;
+
+		static void Main(string[] args) {
+			// For example, a local variable created in Main, is only accessible inside Main.
+			string variable = "I am local to Main.";
+
+			// The line below throws an error, as "anotherVariable" is not an object Main knows of.
+			//Console.WriteLine(anotherVariable);
+
+			classVariable = "I can be accessed there";
+		}
+
+		public static void AnotherMethod() {
+			// AnotherMethod's scope starts here =================
+
+			// A local variable declared inside AnotherMethod is not accessible in Main.
+			// A local variable declared inside Main is not accessible in AnotherMethod.
+
+			string anotherVariable = "I am local to AnotherMethod";
+
+			// The line below throws an error, as "variable" is not an object AnotherMethod knows of.
+			//Console.WriteLine(variable);
+
+			if (true) {
+				// If statement's scope starts here =================
+
+				// The local variable "yetAnotherVariable" is this if statement.
+				// Therefore, it is not accessible outside of this statement.
+				string yetAnotherVariable = "I am accessible inside this if statement, but not outside of it.";
+
+				// However, "anotherVariable" can be accessed in this scope.
+				anotherVariable = "I can be accessed inside this if statement.";
+
+				// If statement's scope ends here =================
+			}
+
+			classVariable = "I can be accessed here too.";
+
+			// AnotherMethod's scope ends here =================
+		}
+	}
+}
---
+++ Example38/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace Example38 {
+	class Program {
+		/*
+
+		Write a console application that simulates an ATM machine.
+
+		When loading the application, users must be presented with a menu that
+		displays the following options:
+
+		1. Deposit;
+		2. Withdraw.
+
+		The starting balance should be zero. From there, users should be able to
+		perform deposits or withdrawals, based on menu selections.
+
+		Once an option is selected, it should prompt the user to input the amount,
+		and then add or subtract that amount from the account balance accordingly.
+
+		*/
+
+		static double balance = 0;
+
+		static void Main(string[] args) {
+			while (true) {
+				Console.Clear();
+
+				bool success = false;
+				double amount;
+
+				while (true) {
+					int input = DisplayMenu();
+
+					if (input == 1) {
+						amount = GetTransactionAmount();
+						success = Deposit(amount);
+
+						break;
+
+					}
+					else if (input == 2) {
+						amount = GetTransactionAmount();
+						success = Withdraw(amount);
+
+						break;
+					}
+					else {
+						DisplayMessage("Invalid selection. Please try again.", true);
+					}
+				}
+
+				if (success) {
+					DisplayMessage($"Transaction successfully processed!");
+				}
+				else {
+					DisplayMessage($"Error processing transaction.", true);
+				}
+
+				Console.WriteLine($"Account balance: {balance:C}\n");
+
+				Console.WriteLine("Would you like to do another transaction? (Y/N)");
+				string tryAgain = Console.ReadLine();
+
+				if (tryAgain.ToUpper() == "Y") {
+					continue;
+				}
+				else if (tryAgain.ToUpper() == "N") {
+					break;
+				}
+				else {
+					DisplayMessage("Invalid selection. Please try again.", true);
+				}
+			}
+		}
+
+		public static int DisplayMenu() {
+			Console.WriteLine("==============================================");
+			Console.WriteLine("||              ATM SIMULATOR               ||");
+			Console.WriteLine("==============================================");
+			Console.WriteLine("\nPlease select an option: ");
+			Console.WriteLine("1. Deposit");
+			Console.WriteLine("2. Withdraw");
+
+			int selection = int.Parse(Console.ReadLine());
+
+			return selection;
+		}
+
+		public static double GetTransactionAmount() {
+			Console.WriteLine("Please input the transaction amount: ");
+			double amount = double.Parse(Console.ReadLine());
+
+			return amount;
+		}
+
+		public static bool Deposit(double amount) {
+			if (amount < 0) {
+				DisplayMessage($"Deposit amount cannot be negative.", true);
+
+				return false;
+			}
+
+			balance += amount;
+
+			return true;
+		}
+
+		public static bool Withdraw(double amount) {
+			if (amount < 0) {
+				DisplayMessage($"Withdrawal amount cannot be negative.", true);
+
+				return false;
+			}
+
+			if (balance >= amount) {
+				balance -= amount;
+
+				return true;
+			}
+
+			return false;
+		}
+
+		public static void DisplayMessage(string message, bool isError = false) {
+			if (isError) {
+				Console.ForegroundColor = ConsoleColor.Red;
+			}
+			else {
+				Console.ForegroundColor = ConsoleColor.Green;
+			}
+
+			Console.WriteLine($"{message}\n");
+			Console.ResetColor();
+		}
+	}
+}
